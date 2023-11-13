76048f6e80914302a5cdd1c548ddbc6e21dd4fff
LukeEn <lukeen@fincheck.com>
2023-11-13 17:16:13 +0700

    update store_cafe

---
+++ database/migrations/2023_11_13_095758_create_carts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_product');
+            $table->integer('price_product');
+            $table->integer('amount');
+            $table->integer('image_product');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carts');
+    }
+};
